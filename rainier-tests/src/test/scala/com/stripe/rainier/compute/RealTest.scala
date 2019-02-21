91cdbe158b3863539358bc39f5eb7900718ee3c4
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2019-02-21 13:05:13 +0100

    Add tests for autodiff that compare to numeric diff

--- rainier-tests/src/test/scala/com/stripe/rainier/compute/RealTest.scala
+++ rainier-tests/src/test/scala/com/stripe/rainier/compute/RealTest.scala
@@ -8,23 +8,52 @@ class RealTest extends FunSuite {
     test(description) {
       val x = new Variable
       val result = fn(x)
+      val deriv = result.gradient.head
+
+      def evalAt(d: Double): Double = Try { fn(Constant(d)) } match {
+        case Success(Infinity)                 => 1.0 / 0.0
+        case Success(NegInfinity)              => -1.0 / 0.0
+        case Success(Constant(bd))             => bd.toDouble
+        case Failure(_: ArithmeticException)   => 0.0 / 0.0
+        case Failure(_: NumberFormatException) => 0.0 / 0.0
+        case x                                 => sys.error("Non-constant value " + x)
+      }
+
       val c = Compiler(200, 100).compile(List(x), result)
+      val dc = Compiler(200, 100).compile(List(x), deriv)
       List(1.0, 0.0, -1.0, 2.0, -2.0, 0.5, -0.5).foreach { n =>
-        val constant = Try { fn(Constant(n)) } match {
-          case Success(Infinity)                 => 1.0 / 0.0
-          case Success(NegInfinity)              => -1.0 / 0.0
-          case Success(Constant(bd))             => bd.toDouble
-          case Failure(_: ArithmeticException)   => 0.0 / 0.0
-          case Failure(_: NumberFormatException) => 0.0 / 0.0
-          case x                                 => sys.error("Non-constant value " + x)
-        }
+        val constant = evalAt(n)
         val eval = new Evaluator(Map(x -> n))
         val withVar = eval.toDouble(result)
         assertWithinEpsilon(constant, withVar, s"[c/ev, n=$n]")
         val compiled = c(Array(n))
         assertWithinEpsilon(withVar, compiled, s"[ev/ir, n=$n]")
+
+        // derivatives of automated differentiation vs numeric differentiation
+        if (n != 0.0) { // exclude 0 since a lot of the test inputs are not defined or not continuous there
+          val dx = 10E-6
+          val numDiff = (evalAt(n + dx) - evalAt(n)) / dx
+          val diffWithVar = eval.toDouble(deriv)
+          assertAlmostEqual(numDiff,
+                            diffWithVar,
+                            s"[numDiff/diffWithVar, n=$n]")
+          val diffCompiled = dc(Array(n))
+          assertAlmostEqual(diffWithVar,
+                            diffCompiled,
+                            s"[numDiffWithVar/diffCompiled, n=$n]")
+        }
       }
     }
+  }
+
+  def assertAlmostEqual(x: Double,
+                        y: Double,
+                        clue: String,
+                        tolerance: Double = 0.001): Unit = {
+    val relativeError = (x - y).abs
+    if (!(x.isNaN && y.isNaN || relativeError < tolerance))
+      assert(x == y, clue)
+    ()
   }
 
   def assertWithinEpsilon(x: Double, y: Double, clue: String): Unit = {
@@ -49,6 +78,9 @@ class RealTest extends FunSuite {
   run("temp") { x =>
     val t = x * 3
     t + t
+  }
+  run("max") { x =>
+    x.max(0)
   }
   run("normal") { x =>
     Real.sum(Range.BigDecimal(0d, 2d, 1d).toList.map { y =>
